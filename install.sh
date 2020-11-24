790b1008e0fade62f9056b2b4ec8053a5e2cc078
iamviv3k <vivek.verma662ha@gmail.com>
2020-11-24 21:37:42 +0530

    remove already installed dotfiles

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo rm -rf ~/.vim > /dev/null 2>&1
+sudo rm -rf ~/.vimrc > /dev/null 2>&1
+sudo rm -rf ~/.bashrc > /dev/null 2>&1
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.vim ~/.vim
