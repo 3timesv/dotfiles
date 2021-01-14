81226f2cff456ff356484b59dc1a8cbd9b0ab1b3
3timesv <vivek.verma662ha@gmail.com>
2021-01-14 10:17:45 +0530

    added .tmux.conf in install script

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@
 sudo rm -rf ~/.vim > /dev/null 2>&1
 sudo rm -rf ~/.vimrc > /dev/null 2>&1
 sudo rm -rf ~/.bashrc > /dev/null 2>&1
+sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 echo "Installing vim plugins..."
 vim +PlugInstall +qall
